7c015ac9d41d1ed24472f8b66a52df50bb70c592
DanielLopez <efrain.loapz@gmail.com>
2023-05-16 17:21:43 -0600

    Agregando logica y un poco de estetica al to do

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 let time = 25 * 60;
 let timer = null;
 let currentTime = null;
+let task = [];
 const minutes = parseInt(time / 60);
 const seconds = parseInt(time % 60);
 
 let myBtnStart = document.getElementById('start');
 let myBtnPause = document.getElementById('pause');
+let myDivTaks = document.getElementById('to-do-tasks');
+let idTask = document.getElementById("idTask");
+let formModal = document.querySelector("#form");
+let mybtnSave = document.getElementById('save');
 
 let timerString = document.getElementById('timerString');
     timerString.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
@@ -15,12 +20,22 @@ let timerString = document.getElementById('timerString');
 
 myBtnStart.addEventListener('click', start);
 myBtnPause.addEventListener('click', pause);
+// myDivTaks.addEventListener('click', clickDivTask);
+formModal.addEventListener('submit', e => {
+    e.preventDefault();
+    if (idTask.value != ""){
+        createTasks(idTask.value);
+        idTask.value = "";
+        renderTask();
+    }
+});
+
 
 function start(){
+    clearInterval(timer);
     timer = setInterval(() =>{
         timerHandler();
     }, 1000);
-
 }
 function pause(){
     clearInterval(timer)
@@ -42,3 +57,35 @@ function renderTime(){
 
 }
 
+// To-do de tasks
+
+
+function createTasks(value){
+    const newtask = {
+        id: Math.round((Math.random() * 100)),
+        title: value,
+        completed: false
+    };
+
+    task.unshift(newtask);
+
+}
+
+function renderTask(){
+    let html = task.map(task =>{
+        return ` 
+        <div class="task">
+            <div class="completed">${task.completed ? `<span class="done"> Done </span>` : `<span class="progress"> In Progress... </span>`}</div>
+            <div class="title">${task.title}</div>
+            <div><button class="btn-taskModal" >
+                <img src="./img/threedots-white.png" alt="imagen de tres puntos" >
+            </button></div>
+        </div>
+        
+        `;
+    });
+
+    let taskContainer = document.querySelector("#CONTASK");
+    taskContainer.innerHTML = html.join("");
+    // myDivTaks.innerHTML = html.join(" ");
+}
